ed3aa877fbb00c5d05d5f736aedf750f2e480513
Alena Popova <olyonapopova@gmail.com>
2021-01-02 01:16:27 +0300

    add 1 task

--- 1.py
+++ 1.py
@@ -1,5 +1,12 @@
-print('hello')
-a = 5
-b = 6
-if a>b:
-    print('world')
+print('Convert RUB / USD ')
+
+def convert (rub, rate):
+    try:
+        rub = float(input('How many rubles do you want to convert to euros '))
+        rate = float(input(('What is the currenst exchange rate ')))
+        return rub / rate
+    except ZeroDivisionError:
+        print('Incorrect input data')
+        exit()
+
+print('You have {:.3} euros in your account'.format(convert(0,0)))
