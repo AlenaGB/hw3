5ca45ad105bbbe9f13fb70a59d2b1e16eb2f1524
Alena Popova <olyonapopova@gmail.com>
2021-01-02 14:12:54 +0300

    added a second task

--- 2.py
+++ 2.py
@@ -1,3 +1,10 @@
-print("number 2")
-a=3
-b=4
+def student():
+    person = {'Name':'', 'Surname':'','Birthday':'','City':'', 'Email':'','Telephon number':'' }
+    for f in person.keys():
+        value = input(f'Enter your {f} ')
+        person[f] = str(value)
+
+    print('My name is', person['Name'],person['Surname'], 'I was born in', person['City'], person['Birthday'], 'My contacts:', person['Email'], person['Telephon number'])
+
+print('Questionnaire')
+student()
