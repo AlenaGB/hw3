4a8fb4bd3232b8eaf12a1c4b116ce1d279bec7e2
Alena Popova <olyonapopova@gmail.com>
2021-01-03 12:45:58 +0300

    added a function in task 5

--- 5.py
+++ 5.py
@@ -1,11 +1,25 @@
 print('Sum of numbers'.upper())
+# реализация с циклом
 
-summa = 0
+# summa = 0
+# try:
+#     while True:
+#         for number in map(int, input('Enter your number or any symbol to stop: ').split()):
+#             summa += number
+#         print(summa)
+# except ValueError:
+#     print('result: ', summa)
 
+#  реализация с функцией
+def listsum(numList):
+    for i in numList:
+        global theSum
+        theSum = theSum + i
+
+theSum = 0
 try:
     while True:
-        for number in map(int, input('Enter your number or any symbol to stop: ').split()):
-            summa += number
-        print(summa)
+        result = (listsum(map(int, input('Enter your number or any symbol to stop: ').split())))
+        print(theSum)
 except ValueError:
-    print('result: ', summa)
+    print('result: ', theSum)
